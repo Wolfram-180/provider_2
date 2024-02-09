f12d6e8906688832f2690a40d760600e721028ff
Wolfram-180 <sergei.ulvis@gmail.com>
2024-02-09 20:21:02 +0300

    streams

--- lib/main.dart
+++ lib/main.dart
@@ -44,3 +44,54 @@ class BaseObject {
   @override
   int get hashCode => id.hashCode;
 }
+
+@immutable
+class ExpensiveObject extends BaseObject {
+  //
+}
+
+@immutable
+class CheapObject extends BaseObject {
+  //
+}
+
+class ObjectProvider extends ChangeNotifier {
+  late String id;
+  late CheapObject _cheapObject;
+  late StreamSubscription _cheapObjectStreamSubs;
+  late ExpensiveObject _expensiveObject;
+  late StreamSubscription _expensiveObjectStreamSubs;
+
+  CheapObject get cheapObject => _cheapObject;
+  ExpensiveObject get expensiveObject => _expensiveObject;
+
+  ObjectProvider()
+      : id = const Uuid().v4(),
+        _cheapObject = CheapObject(),
+        _expensiveObject = ExpensiveObject();
+
+  void start() {
+    _cheapObjectStreamSubs = Stream.periodic(
+      const Duration(seconds: 1),
+    ).listen(
+      (_) {
+        _cheapObject = CheapObject();
+        notifyListeners();
+      },
+    );
+
+    _expensiveObjectStreamSubs = Stream.periodic(
+      const Duration(seconds: 10),
+    ).listen(
+      (_) {
+        _expensiveObject = ExpensiveObject();
+        notifyListeners();
+      },
+    );
+  }
+
+  void stop() {
+    _cheapObjectStreamSubs.cancel();
+    _expensiveObjectStreamSubs.cancel();
+  }
+}
