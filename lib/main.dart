3fa27cb6eeeebc524bd241bec619ea3a55e92b0a
Wolfram-180 <sergei.ulvis@gmail.com>
2024-02-09 17:44:03 +0300

    class BaseObject

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(
@@ -24,4 +27,20 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+}
+
+@immutable
+class BaseObject {
+  final String id;
+  final String lastUpdated;
+
+  BaseObject()
+      : id = const Uuid().v4(),
+        lastUpdated = DateTime.now().toIso8601String();
+
+  @override
+  bool operator ==(covariant BaseObject other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
 }
